43d43574118761855bfa2d47bfe33cc4bffc3f65
Artur Gilmutdinov <arturg000@gmail.com>
2013-06-25 00:08:46 +0400

    hdivider cache clearing implemented

--- hdivider/hdivider_dist/hdivider_state_accessor_cache.cpp
+++ hdivider/hdivider_dist/hdivider_state_accessor_cache.cpp
@@ -14,6 +14,7 @@ HdividerStatesCache::~HdividerStatesCache()
     delete this->cache;
 }
 
+// if handled dont keep state in cache
 InputState* HdividerStatesCache::getState(int64_t input_id)
 {
     pthread_mutex_lock(&mutex);
@@ -22,13 +23,30 @@ InputState* HdividerStatesCache::getState(int64_t input_id)
     tr1::unordered_map<int64_t, InputState*>::iterator it = cache->find(input_id);
     if (it!=cache->end())
     {
-        return new InputState(*it->second);
+        if (it->second->handled)
+        {
+            InputState *state = new InputState(*it->second);
+            delete it->second;
+            cache->erase(it);
+            return state;
+        }
+        else
+        {
+            return new InputState(*it->second);
+        }
     }
     else
     {
         InputState *state = mongo_accessor->getState(input_id);
-        cache->insert(pair<int64_t, InputState*>(input_id, state));
-        return new InputState(*state);
+        if (state->handled)
+        {
+            return state;
+        }
+        else
+        {
+            cache->insert(pair<int64_t, InputState*>(input_id, state));
+            return new InputState(*state);
+        }
     }
 }
 
@@ -43,13 +61,24 @@ void HdividerStatesCache::saveState(const InputState *state)
     tr1::unordered_map<int64_t, InputState*>::iterator it = cache->find(state->id);
     if (it!=cache->end())
     {
-        delete it->second;
-        it->second = new InputState(*state);
+        if (state->handled)
+        {
+            delete it->second;
+            cache->erase(it);
+        }
+        else
+        {
+            delete it->second;
+            it->second = new InputState(*state);
+        }
     }
     else
     {
-        InputState *new_state = new InputState(*state);
-        cache->insert(pair<int64_t, InputState*>(state->id, new_state));
+        if (!state->handled)
+        {
+                InputState *new_state = new InputState(*state);
+                cache->insert(pair<int64_t, InputState*>(state->id, new_state));
+        }
     } 
 }
 
