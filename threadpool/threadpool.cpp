86b7313f0c1f830cf8b913d6f4598c7d63ff9e46
a.gilmutdinov <a.gilmutdinov@arturg.mediatoday>
2013-09-27 14:49:16 +0400

    Added the BSD 2 clause license

--- threadpool/threadpool.cpp
+++ threadpool/threadpool.cpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2010-2013  Artur Gilmutdinov
+
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the BSD 2-Clause License
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * BSD 2-Clause License for more details.
+
+ * You should have received a copy of the BSD 2-Clause License
+ * along with this program; if not, see <http://opensource.org/licenses>.
+
+ * See also <http://highinit.com>
+*/
+
 #include "threadpool.h"
 
 hThread::hThread(CallBackQueue task_queue, ThreadQueue waiting_threads, pthread_t &th)
