30aa5bc60f3b5831d4a97fc0beb990519ffdd686
Artur Gilmutdinov <arturg000@gmail.com>
2014-07-21 11:01:36 +0400

    Added HttpSimpleRequesterPost

---
+++ network/HttpSimpleRequesterPost.cpp
@@ -0,0 +1,36 @@
+#include "HttpSimpleRequesterPost.h"
+
+HttpSimpleRequesterPost::HttpSimpleRequesterPost(boost::function<void(int, int, const std::string&)> _onCall,
+							boost::function<void(int, int, const std::string&, const std::string&)> _onCallPost,
+							boost::function<void(int)> _onFinished,
+							CallContextPtr _context,
+							const std::string &_call_url,
+							const std::string &_postdata,
+							boost::function<void(CallContextPtr, const std::string&)> _onCalledOk,
+							boost::function<void(CallContextPtr)> _onCalledFail):
+
+		HttpOutRequestDisp::Requester(_onCall, _onCallPost, _onFinished),
+		m_context(_context),
+		m_call_url(_call_url),
+		m_postdata(_postdata),
+		m_onCalledOk(_onCalledOk),
+		m_onCalledFail(_onCalledFail) {
+
+}
+
+void HttpSimpleRequesterPost::onCallDone (int _callid, bool _success, const std::string &_resp) {
+	
+	if (!_success) {
+		m_onCalledFail(m_context);
+		finished();
+	}
+	else {
+		m_onCalledOk(m_context, _resp);
+		finished();
+	}
+}
+
+void HttpSimpleRequesterPost::start() {
+		
+	callPost(0, m_call_url, m_postdata);
+}
--- network/HttpSimpleRequesterPost.h
+++ network/HttpSimpleRequesterPost.h
@@ -6,22 +6,33 @@
 #include "hiaux/network/HttpSrv.h"
 #include "hiaux/network/CallContext.h"
 
-class HttpSimpleRequester : public HttpOutRequestDisp::Requester {
+/*
+class CallContextPost {
+
+public:
+	virtual ~CallContextPost() { }
+};
+
+typedef boost::shared_ptr<CallContextPost> CallContextPPtr;
+*/
+class HttpSimpleRequesterPost : public HttpOutRequestDisp::Requester {
 public:	
 
 private:
 	std::string m_call_url;
+	std::string m_postdata;
 	boost::function<void(CallContextPtr, const std::string&)> m_onCalledOk;
 	boost::function<void(CallContextPtr)> m_onCalledFail;
 	CallContextPtr m_context;
 
 public:
 	
-	HttpSimpleRequester(boost::function<void(int, int, const std::string&)> _onCall,
-						boost::function<void(int, int, const std::string&, const std::string&)> _onCallPost,
+	HttpSimpleRequesterPost(boost::function<void(int, int, const std::string&)> _onCall,
+							boost::function<void(int, int, const std::string&, const std::string&)> _onCallPost,
 							boost::function<void(int)> _onFinished,
 							CallContextPtr _context,
 							const std::string &_call_url,
+							const std::string &_postdata,
 							boost::function<void(CallContextPtr, const std::string&)> _onCalledOk,
 							boost::function<void(CallContextPtr)> _onCalledFail);
 	
